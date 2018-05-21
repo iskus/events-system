ccd5182bb0296302a11c28e369e9e658011f4747
Nazi <nazi@localhost.localdomain>
2018-05-21 13:51:05 +0300

    Added mediator

---
+++ EventManager.php
@@ -0,0 +1,15 @@
+<?php
+class EventManager {
+    protected $events = array();
+    public function attach($eventName, $callback) {
+        if (!isset($this->events[$eventName])) {
+            $this->events[$eventName] = array();
+        }
+        $this->events[$eventName][] = $callback;
+    }
+    public function trigger($eventName, $data = null) {
+        foreach ($this->events[$eventName] as $callback) {
+            $callback($eventName, $data);
+        }
+    }
+}
--- index.php
+++ index.php
@@ -8,16 +8,28 @@ function test($tag, $data)
     EventProcessor::init($tag, $data);
 }
 
-test('book.block', [
+$blockBookData = [
     'moder_id' => 1,
     'author_id' => 2,
     'book_id' => 3,
     'comment' => 'hgdmgm  kht dkdj',
-]);
-test('login.warning', ['user_id' => 5, 'subject' => 'Wron Pass']);
-test('ticket.created', [
+];
+$loginWarningData = ['user_id' => 5, 'subject' => 'Wron Pass'];
+$ticketData = [
     'user_id' => 76,
     'sum' => 8788,
     'currency' => 'USD',
     'account' => 'jhchghgjjgc',
-]);
+];
+test('book.block', $blockBookData);
+test('login.warning', $loginWarningData);
+test('ticket.created', $ticketData);
+
+$eventManager = new EventManager;
+$eventManager->attach('book.block', function() use ($blockBookData) { echo "system user / "; });
+$eventManager->attach('login.warning', function() use ($loginWarningData) { echo "email user / "; });
+$eventManager->attach('ticket.created', function() use ($ticketData) { echo "email admin / "; });
+$eventManager->attach('ticket.created', function() use ($ticketData) { echo "email user / "; });
+$eventManager->trigger('login.warning');
+$eventManager->trigger('book.block');
+$eventManager->trigger('ticket.created');
